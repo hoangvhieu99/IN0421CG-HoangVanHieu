4674036e13c58db6514b7e1971f54e55cad0e4d5
hoangvhieu99 <hieuvanhoang99@gmail.com>
2021-04-20 16:40:51 +0700

    up file sql

---
+++ _01_database/advance.sql
@@ -0,0 +1,454 @@
+create database QL_NghiDuong;
+use QL_NghiDuong;
+create table ViTri(
+	idvitri int primary key,
+    tenvitri varchar(45)
+);
+
+INSERT INTO ViTri(idvitri,tenvitri) values(2,'Quản Lý');
+INSERT INTO ViTri(idvitri,tenvitri) values(5,'Phục Vụ');
+INSERT INTO ViTri(idvitri,tenvitri) values(1,'Giám đốc');
+INSERT INTO ViTri(idvitri,tenvitri) values(3,'Giám sát');
+INSERT INTO ViTri(idvitri,tenvitri) values(4,'Chuyên viên');
+INSERT INTO ViTri(idvitri,tenvitri) values(6,'Lễ Tân');
+select*from ViTri;
+create table TrinhDo(
+	idtrinhdo int primary key,
+    trinhdo varchar(45)
+);
+INSERT INTO TrinhDo(idtrinhdo,trinhdo) values(0,'Sau Đai Học');
+INSERT INTO TrinhDo(idtrinhdo,trinhdo) values(1,'Đại Học');
+INSERT INTO TrinhDo(idtrinhdo,trinhdo) values(2,'Cao Đẳng');
+INSERT INTO TrinhDo(idtrinhdo,trinhdo) values(3,'Trung Cấp');
+select *from TrinhDo;
+create table BoPhan(
+	idbophan int primary key,
+    tenbophan varchar(45)
+);
+INSERT INTO BoPhan(idbophan,tenbophan) values(1,'Quản lý');
+INSERT INTO BoPhan(idbophan,tenbophan) values(2,'Phục Vụ');
+INSERT INTO BoPhan(idbophan,tenbophan) values(3,'Hành Chính');
+INSERT INTO BoPhan(idbophan,tenbophan) values(4,'Sale-Marketing');
+select * from BoPhan;
+create table NhanVien(
+	idnhanvien int primary key,
+    Hoten int,
+    idvitri int,
+    idtrinhdo int,
+    idbophan int,
+    ngaysinh date,
+    socmnd varchar(45),
+    luong varchar(45),
+    sdt varchar(45),
+    email varchar(45),
+    diachi varchar(45),
+    foreign key (idvitri ) references ViTri(idvitri),
+    foreign key (idtrinhdo) references TrinhDo(idtrinhdo),
+    foreign key (idbophan) references BoPhan(idbophan)
+);
+ALTER TABLE NhanVien Modify Hoten varchar(45);
+ALTER TABLE NhanVien change Hoten hoten varchar(45);
+insert into NhanVien(idnhanvien,Hoten,idvitri,idtrinhdo,idbophan,ngaysinh,socmnd,luong,sdt,email,diachi)
+values(1001,'Hoàng Văn Hiếu',5,2,1,'1999/01/06','197405491','15000000','0987654312','hieuvanhoang99@gmail.com','Quảng Trị');
+insert into NhanVien(idnhanvien,Hoten,idvitri,idtrinhdo,idbophan,ngaysinh,socmnd,luong,sdt,email,diachi)
+values(1002,'Nguyễn Huấn',5,1,2,'1999/08/24','197015746','15000000','0985123456','huannguyen99@gmail.com','Đà Nẵng');
+insert into NhanVien(idnhanvien,Hoten,idvitri,idtrinhdo,idbophan,ngaysinh,socmnd,luong,sdt,email,diachi)
+values(1003,'Nguyễn Văn Tùng',5,3,4,'1999/06/12','197012478','12000000','0905123456','tungnguyen99@gmail.com','Quảng Nam');
+insert into NhanVien(idnhanvien,Hoten,idvitri,idtrinhdo,idbophan,ngaysinh,socmnd,luong,sdt,email,diachi)
+values(1004,'Trần Tuấn Kiệt',5,1,3,'1999/12/24','197016987','15000000','0905123456','kiettran99@gmail.com','Quảng Trị');
+select * from NhanVien;
+create table DichVuDiKem(
+	iddichvudikem int primary key,
+	tendichvudikem varchar(45),
+    gia int,
+    donvi int,
+    trangthaikhadung varchar(45)
+);
+ALTER TABLE DichVuDiKem Modify donvi varchar(45);
+insert into DichVuDiKem(iddichvudikem, tendichvudikem,gia,donvi,trangthaikhadung)
+values(1,'Thuê xe di chuyển tham quan resort',150000,'Giờ','Khả Dụng');
+insert into DichVuDiKem(iddichvudikem, tendichvudikem,gia,donvi,trangthaikhadung)
+values(2,'Nước uống',15000,'Chai','Khả Dụng');
+insert into DichVuDiKem(iddichvudikem, tendichvudikem,gia,donvi,trangthaikhadung)
+values(3,'Thức ăn',150000,'Tuỳ Loại','Khả Dụng');
+insert into DichVuDiKem(iddichvudikem, tendichvudikem,gia,donvi,trangthaikhadung)
+values(4,'Karaoke',150000,'Giờ','Khả Dụng');
+insert into DichVuDiKem(iddichvudikem, tendichvudikem,gia,donvi,trangthaikhadung)
+values(5,'Massage',300000,'Tuỳ Loại','Khả Dụng');
+select * from DichVuDiKem;
+create table LoaiKhach(
+	idloaikhach int primary key,
+    tenloaikhach varchar(45)
+);
+insert into LoaiKhach(idloaikhach,tenloaikhach) values (1,'Diamond');
+insert into LoaiKhach(idloaikhach,tenloaikhach) values (2,'Platinium');
+insert into LoaiKhach(idloaikhach,tenloaikhach) values (3,'Gold');
+insert into LoaiKhach(idloaikhach,tenloaikhach) values (4,'Silver');
+insert into LoaiKhach(idloaikhach,tenloaikhach) values (5,'Member');
+select * from LoaiKhach;
+create table KieuThue(
+	idkieuthue int primary key,
+    tenkieuthue varchar(45),
+    gia int
+);
+insert into KieuThue(idkieuthue,tenkieuthue,gia)values(0,'Thuê Theo Năm',1200000000);
+insert into KieuThue(idkieuthue,tenkieuthue,gia)values(1,'Thuê Theo Tháng',30000000);
+insert into KieuThue(idkieuthue,tenkieuthue,gia)values(2,'Thuê Theo Ngày',1000000);
+insert into KieuThue(idkieuthue,tenkieuthue,gia)values(3,'Thuê Theo Giờ',200000);
+select * from KieuThue;
+create table LoaiDichVu(
+	idloaidichvu int primary key,
+    tenloaidichvu varchar(45)
+);
+insert into LoaiDichVu(idloaidichvu,tenloaidichvu)values(1,'Villa');
+insert into LoaiDichVu(idloaidichvu,tenloaidichvu)values(2,'House');
+insert into LoaiDichVu(idloaidichvu,tenloaidichvu)values(3,'Room');
+select * from LoaiDichVu;
+create table KhachHang(
+	idkhachhang int primary key,
+    idloaikhach int,
+    hoten varchar(45),
+    ngaysinh date,
+    socmnd varchar(45),
+    sdt varchar(45),
+    email varchar(45),
+    diachi varchar(45),
+    foreign key (idloaikhach) references LoaiKhach(idloaikhach)
+);
+insert into KhachHang(idkhachhang,idloaikhach,hoten,ngaysinh ,socmnd,sdt,email,diachi)
+values(101,1,'Hoàng Bút','1935/06/09','16581237','0918123789','buthoang@gmail.com','Quảng Trị');
+insert into KhachHang(idkhachhang,idloaikhach,hoten,ngaysinh ,socmnd,sdt,email,diachi)
+values(102,2,'Hoàng Thị Lựu','1945/12/09','13581275','0935555555','buthoang@gmail.com','Triệu Đông');
+insert into KhachHang(idkhachhang,idloaikhach,hoten,ngaysinh ,socmnd,sdt,email,diachi)
+values(103,4,'Lê Thị Họng','1960/06/09','19763514','0908888668','hongle@gmail.com','Triệu Thành');
+insert into KhachHang(idkhachhang,idloaikhach,hoten,ngaysinh ,socmnd,sdt,email,diachi)
+values(104,3,'Lê Tập','1989/06/09','18546354','0906666666','letap@gmail.com','Hà Nội');
+insert into KhachHang(idkhachhang,idloaikhach,hoten,ngaysinh ,socmnd,sdt,email,diachi)
+values(105,1,'Hoàng Duc','1935/06/09','175821463','0982455555','duchoang@gmail.com','Đà Nẵng');
+select *from KhachHang;
+
+create table DichVu(
+	iddichvu int primary key,
+	tendichvu varchar(45),
+    dientich int,
+    sotang int,
+    songuoitoida varchar(45),
+    chiphithue varchar(45),
+    idkieuthue int,
+    idloaidichvu int,
+    trangthai varchar(45),
+    constraint foreign key (idkieuthue) references KieuThue(idkieuthue),
+    constraint foreign key (idloaidichvu) references LoaiDichVu(idloaidichvu)
+);
+insert into DichVu(iddichvu,tendichvu,dientich,sotang,songuoitoida,chiphithue,idkieuthue,idloaidichvu,trangthai)
+values(1,'Villa',90,2,'10 người','1000000',2,1,'Chưa Sử Dụng');
+insert into DichVu(iddichvu,tendichvu,dientich,sotang,songuoitoida,chiphithue,idkieuthue,idloaidichvu,trangthai)
+values(2,'Room',40,1,'4 người','300000',3,3,'Đang Sử Dụng');
+insert into DichVu(iddichvu,tendichvu,dientich,sotang,songuoitoida,chiphithue,idkieuthue,idloaidichvu,trangthai)
+values(3,'House',80,2,'6 người','120000000',0,2,'Đã Sử Dụng');
+select *from DichVu;
+create table HopDong(
+	idhopdong int primary key,
+	idnhanvien int,
+    idkhachhang int,
+    iddichvu int,
+    ngaylamhopdong date,
+    ngayketthuc date,
+    tiendatcoc int,
+    tongtien int,
+    constraint foreign key (idnhanvien) references NhanVien(idnhanvien),
+    constraint foreign key (idkhachhang) references KhachHang(idkhachhang),
+    constraint foreign key (iddichvu) references DichVu(iddichvu)
+);
+insert into HopDong(idhopdong,idnhanvien,idkhachhang,iddichvu,ngaylamhopdong,ngayketthuc,tiendatcoc,tongtien)
+values(001,1001,101,2,'2019/12/01','2019/12/01',300000,300000);
+insert into HopDong(idhopdong,idnhanvien,idkhachhang,iddichvu,ngaylamhopdong,ngayketthuc,tiendatcoc,tongtien)
+values(002,1002,101,1,'2020/07/05','2020/07/06',500000,1000000);
+insert into HopDong(idhopdong,idnhanvien,idkhachhang,iddichvu,ngaylamhopdong,ngayketthuc,tiendatcoc,tongtien)
+values(003,1004,101,3,'2018/12/01','2019/11/30',30000000,120000000);
+select *from HopDong;
+
+create table HopDongChiTiet(
+	idhopdongchitiet int primary key,
+    idhopdong int,
+    iddichvudikem int,
+    soluong int,
+    foreign key (idhopdong) references HopDong(idhopdong),
+    foreign key (iddichvudikem) references DichVuDiKem(iddichvudikem)
+);
+insert into HopDongChiTiet(idhopdongchitiet,idhopdong,iddichvudikem,soluong)
+values(1,001,1,1);
+insert into HopDongChiTiet(idhopdongchitiet,idhopdong,iddichvudikem,soluong)
+values(2,002,3,5);
+insert into HopDongChiTiet(idhopdongchitiet,idhopdong,iddichvudikem,soluong)
+values(3,003,4,2);
+insert into HopDongChiTiet(idhopdongchitiet,idhopdong,iddichvudikem,soluong)
+values(4,001,4,2);
+insert into HopDongChiTiet(idhopdongchitiet,idhopdong,iddichvudikem,soluong)
+values(5,001,3,2);
+select *from HopDongChiTiet;
+
+--- cau2
+select * from nhanvien where Hoten like 'H%_____________' or Hoten like 'T%_____________' or Hoten like 'K%_____________';
+
+--- cau3
+select * from khachhang where  diachi in ('Quảng Trị', 'Đà Nẵng') and year(CURDATE()) - year(ngaysinh) between 18 and 50;
+
+--- cau4
+select a.idkhachhang, a.hoten, count(b.idkhachhang)as solandatphong
+from khachhang a, hopdong b
+where a.idkhachhang=b.idkhachhang and a.idloaikhach = 01
+group by a.hoten
+order by solandatphong asc;
+
+--- cau5
+select kh.idkhachhang, kh.hoten, lk.tenloaikhach, hd.idhopdong,
+dv.tendichvu, hd.ngaylamhopdong, hd.ngayketthuc, (dv.chiphithue+hdct.soluong*dvdk.gia) as TongTien
+from KhachHang as kh  
+left join LoaiKhach as lk on lk.idloaikhach=kh.idloaikhach
+left join hopdong as hd on kh.idkhachhang = hd.idkhachhang
+left join dichvu as dv on dv.iddichvu=hd.iddichvu
+left join hopdongchitiet as hdct on hdct.idhopdong=hd.idhopdong 
+left join dichvudikem as dvdk on dvdk.iddichvudikem=hdct.iddichvudikem;
+
+--- cau6
+select dv.iddichvu,dv.tendichvu,dv.dientich,dv.chiphithue,ldv.tenloaidichvu,hd.ngaylamhopdong
+from dichvu as dv   
+ join hopdong as hd on dv.iddichvu = hd.iddichvu 
+ join loaidichvu as ldv on dv.idloaidichvu=ldv.idloaidichvu
+WHERE ngaylamhopdong <='2019/1/1';
+
+--- cau7
+select a.iddichvu,tendichvu,dientich,songuoitoida,chiphithue,b.tenloaidichvu
+from dichvu a, loaidichvu b, hopdong c, khachhang d
+where a.idloaidichvu=b.idloaidichvu and
+a.iddichvu=c.iddichvu and
+c.idkhachhang=d.idkhachhang and
+year(ngaylamhopdong) =2018 and year(ngaylamhopdong)<2019;
+
+--- cau8
+SELECT DISTINCT hoten FROM khachhang;
+SELECT hoten FROM khachhang GROUP BY hoten HAVING COUNT(idkhachhang) >= 1;
+SELECT hoten FROM khachhang UNION DISTINCT SELECT hoten FROM khachhang;
+
+--- cau9
+select month(ngaylamhopdong) as Thang, count(idkhachhang) as khachhang
+from hopdong 
+where year(ngaylamhopdong) = 2019  group by Thang order by Thang asc;
+
+--- cau10
+select hopdong.idhopdong, hopdong.ngaylamhopdong, hopdong.ngayketthuc, hopdong.tiendatcoc, dichvudikem.iddichvudikem
+from ((hopdongchitiet
+		inner join hopdong on hopdongchitiet.idhopdong = hopdong.idhopdong)
+        inner join dichvudikem on hopdongchitiet.iddichvudikem = dichvudikem.iddichvudikem)
+        having count(dichvudikem.iddichvudikem);
+
+--- cau11
+select a.iddichvudikem, a.tendichvudikem, a.gia, a.donvi, a.trangthaikhadung
+from dichvudikem as a  
+left join hopdongchitiet as d on a.iddichvudikem=d.iddichvudikem  
+left join hopdong as e on e.idhopdong=d.idhopdong  
+left join khachhang as c on c.idkhachhang=e.idkhachhang
+left join loaikhach as b on b.idloaikhach=c.idloaikhach
+where c.diachi like '%Vinh%' or c.diachi like '%Quảng Ngãi%';
+
+--- cau12 
+SELECT a.idhopdong, a.ngaylamhopdong , b.Hoten as TenNhanVien, c.Hoten as TenKhachHang, c.sdt as SDTKhachHang, d.tendichvu, a.tiendatcoc 
+FROM hopdong as a
+inner join nhanvien as b on a.idnhanvien = b.idnhanvien
+inner join khachhang as c on a.idkhachhang = c.idkhachhang
+inner join dichvu as d on a.iddichvu = d.iddichvu
+inner join hopdongchitiet as e on a.idhopdong= e.idhopdong
+inner join dichvudikem as f on f.iddichvudikem = e.iddichvudikem
+where (month(a.ngaylamhopdong) in (10,11,12) and year(a.ngaylamhopdong) = 2019)
+group by a.idhopdong
+;
+
+--- cau13
+select a.iddichvudikem, a.tendichvudikem, c.idkhachhang, d.hoten,count(b.iddichvudikem) as solan
+from dichvudikem a ,hopdongchitiet b ,hopdong c,khachhang d 
+where a.iddichvudikem=b.iddichvudikem and
+ b.idhopdong=c.idhopdong and
+ c.idkhachhang=d.idkhachhang 
+group by a.iddichvudikem, a.tendichvudikem, c.idkhachhang, d.hoten
+having solan =
+(select count(b.iddichvudikem) as solan
+from dichvudikem a ,hopdongchitiet b ,hopdong c,khachhang d 
+where a.iddichvudikem=b.iddichvudikem and
+ b.idhopdong=c.idhopdong and
+ c.idkhachhang=d.idkhachhang 
+group by a.iddichvudikem, a.tendichvudikem, c.idkhachhang, d.hoten
+order by solan desc
+limit 1);
+
+--- cau14
+Select b.idhopdong, c.tenloaidichvu, d.tendichvudikem, count(a.iddichvudikem) as solan
+From hopdongchitiet a,hopdong b, loaidichvu c, dichvudikem d, dichvu e
+where d.iddichvudikem=a.iddichvudikem and
+a.idhopdong=b.idhopdong and 
+e.iddichvu=b.iddichvu and
+c.idloaidichvu= e.idloaidichvu 
+group by b.idhopdong, c.tenloaidichvu, d.tendichvudikem
+having solan=1;
+
+--- cau15
+select c.idnhanvien,Hoten,a.trinhdo,b.tenbophan,sdt,diachi
+from trinhdo a,bophan b,nhanvien c, hopdong d
+where a.idtrinhdo=c.idtrinhdo and
+b.idbophan=c.idbophan  and
+d.idnhanvien = c.idnhanvien
+group by idnhanvien,Hoten,a.trinhdo,b.tenbophan,sdt,diachi
+having count(d.idhopdong)>=3;
+
+--- cau16
+delete from nhanvien nv
+where nv.idnhanvien not in(select  nv.idnhanvien from nhanvien
+inner join hopdong hd on nv.idnhanvien = hd.idnhanvien
+where year(hd.ngaylamhopdong) between 2017 and 2019);
+
+--- cau17
+update khachhang set idloaikhach = 1 where 
+idkhachhang in (select distinct hopdong.idkhachhang from hopdong
+where (select sum(tongtien) from hopdong group by idkhachhang)>10000000 
+and year(ngaylamhopdong) = 2019) and idloaikhach = 2;
+
+--- cau18
+delete from khachhang 
+where khachhang.idkhachhang not in(select distinct khachhang.idkhachhang from khachhang
+inner join hopdong hd on hd.idkhachhang = khachhang.idkhachhang
+where year(hd.ngaylamhopdong) <2020);
+
+--- cau19
+update dichvudikem 
+set gia = gia*2
+where iddichvudikem =
+(select a.iddichvudikem 
+from hopdongchitiet a, hopdong b where 
+soluong<=3 and year(b.ngaylamhopdong) = 2019);
+
+--- cau20
+SELECT idkhachhang as id,hoten,email,sdt,ngaysinh,diachi FROM khachhang
+UNION 
+SELECT idnhanvien as id,hoten,email,sdt,ngaysinh,diachi FROM nhanvien;
+
+--- cau21
+CREATE VIEW V_NHANVIEN AS
+SELECT a.*
+FROM NHANVIEN a inner join hopdong as b on  a.idnhanvien=b.idnhanvien
+where b.ngaylamhopdong='2019/12/12' and a.diachi='Hải Châu';
+
+--- cau22
+update V_NHANVIEN set diachi='Liên Chiểu'
+where diachi='Hải Châu';
+
+--- cau23
+create index IX_KHACHHANG on KhachHang(IDKhachHang);
+-- đối với cơ sở dữ liệu nhỏ không nhìn thấy sự hiệu quả của index
+
+--- cau24
+EXPLAIN select * from khachhang
+where diachi like "%Quảng Trị%" and sdt like "%0908888%";
+
+CREATE UNIQUE INDEX IX_SoDT_DiaChi
+ON khachhang (diachi, sdt);
+
+EXPLAIN select * from khachhang
+where diachi like "%Quảng Trị%" and sdt like "%0906666%";
+
+--- cau25
+DELIMITER //
+CREATE PROCEDURE Sp_1(
+IN id_kh INT,
+OUT message VARCHAR(50)
+)
+IF id_kh in (Select idkhachhang from khachhang) THEN
+BEGIN
+	DELETE FROM khachhang WHERE khachhang.idkhachhang = id_kh;
+    SET message = "Đã xóa khách hàng" ;
+END;
+ELSE
+BEGIN
+SET message = "Khách hàng không tồn tại" ;
+END;
+END IF;
+//  DELIMITER ;
+CALL Sp_1(1001, @message);
+SELECT @message;
+
+--- cau26
+DELIMITER //
+CREATE PROCEDURE Sp_2(in IDhopdong int, IDnhanvien int, IDkhachhang int, IDdichvu int , 
+		Ngaylamhopdong date, Ngayketthuc date, Tiendatcoc int, Tongtien int)
+BEGIN 
+INSERT INTO hopdong
+VALUES ( idhopdong, idnhanvien, idkhachhang , iddichvu , ngaylamhopdong , ngayketthuc , tiendatcoc, tongtien ) ;
+END 
+//  DELIMITER 
+call Sp_2(004,1002,102,2,'2015/01/01','2019/12/31',30000000,120000000)
+
+--- cau27
+create table tonghopdong(
+	total int not null
+);
+INSERT INTO tonghopdong(total)
+SELECT count(idhopdong) 
+FROM hopdong;
+ -----
+CREATE TRIGGER Tr_01
+AFTER DELETE
+ON hopdong FOR EACH ROW
+UPDATE tonghopdong 
+SET total=total-1;
+    
+alter table hopdongchitiet
+add constraint fk_ho
+FOREIGN KEY (idhopdong) REFERENCES hopdong(idhopdong)
+ON DELETE CASCADE;
+
+delete from hopdong
+where id=2;
+select * from tonghopdong;
+
+--- cau29a
+DELIMITER //
+CREATE FUNCTION func_1(
+	tongtien int
+) 
+RETURNS VARCHAR(20)
+DETERMINISTIC
+BEGIN
+    DECLARE dem varchar(20);
+    IF tongtien>2000000 Then 
+    Set dem=1;
+    ELSEIF tongtien < 200000 THEN
+        SET dem = 0;
+    End if;
+    return (dem);
+END//
+DELIMITER ;
+select tongtien,tendichvu ,func_1(tongtien) as solan
+from hopdong a, dichvu b,loaidichvu c
+where a.iddichvu=b.iddichvu
+and  b.idloaidichvu=c.idloaidichvu
+and a.tongtien >2000000;
+
+--- cau29b
+DELIMITER //
+CREATE FUNCTION func_2(
+	idkhachhang int
+) 
+RETURNS VARCHAR(20)
+DETERMINISTIC
+BEGIN
+    DECLARE songay VARCHAR(20);
+    IF idkhachhang > 0 THEN
+		SET songay = ngayketthuc - ngaylamhopdong;
+    END IF;
+	-- return the student level
+	RETURN (songay);
+END//
+DELIMITER ;
+select datediff(ngayketthuc,ngaylamhopdong) as songay
+from hopdong
+order by songay desc
+limit 1;
